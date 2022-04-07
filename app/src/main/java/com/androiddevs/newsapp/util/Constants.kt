3da30d325dd286a6c48d166dcecfd23fdadeeb41
sm02 <starzary@naver.com>
2022-04-07 17:47:34 +0900

    News_App clone Retrofit Setup 까지

---
+++ app/src/main/java/com/androiddevs/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "34bfb6fbcada4e33878e14dd4bcd1c9f"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
