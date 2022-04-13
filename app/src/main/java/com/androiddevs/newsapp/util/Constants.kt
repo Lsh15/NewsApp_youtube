2fe77204abd370540f7fccf85ad94adc0275a622
sm02 <starzary@naver.com>
2022-04-13 15:49:34 +0900

    News_App clone : Searching News 까지

--- app/src/main/java/com/androiddevs/newsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/newsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "34bfb6fbcada4e33878e14dd4bcd1c9f"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
