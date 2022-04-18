94b225b109a8b5ed9543c9ef529d4e72db9f2949
sm02 <starzary@naver.com>
2022-04-18 17:32:26 +0900

    News_App clone : Handling Pagination with Retrofit 까지

--- app/src/main/java/com/androiddevs/newsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/newsapp/util/Constants.kt
@@ -5,5 +5,7 @@ class Constants {
         const val API_KEY = "34bfb6fbcada4e33878e14dd4bcd1c9f"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+
     }
 }
